e7fd418b88b5f2c92914f34049bb99e75b938fd8
Sohag Hossain <sohag.hossain@sebpo.com>
2018-12-11 16:16:12 +0600

    added telescope and done necessary changes

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.16 on 2018-12-10 09:45:11.
+ * Generated for Laravel 5.7.16 on 2018-12-11 09:48:43.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
